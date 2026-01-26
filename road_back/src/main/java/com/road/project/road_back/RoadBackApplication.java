dc63731dd42b809d25414e7719f876359d1b2f67
Kei-Shiro <razafindralambonahary04@gmail.com>
2026-01-26 22:39:22 +0300

    Initial commit

---
+++ road_back/src/main/java/com/road/project/road_back/RoadBackApplication.java
@@ -0,0 +1,13 @@
+package com.road.project.road_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadBackApplication.class, args);
+    }
+
+}
---
+++ road_back/src/test/java/com/road/project/road_back/RoadBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.road.project.road_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
